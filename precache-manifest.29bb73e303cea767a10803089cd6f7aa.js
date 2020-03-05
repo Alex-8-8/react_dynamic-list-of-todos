cd484f9e28fa9204e536b98e559353cdc59b994c
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-03-05 10:26:30 +0200

    Updates

---
+++ precache-manifest.29bb73e303cea767a10803089cd6f7aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3dc053c463a88b68267dd5e143a6ea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f0d075b79ee7f1148f1",
+    "url": "./static/css/main.7fa898f4.chunk.css"
+  },
+  {
+    "revision": "d2f5341ff699db7dd143",
+    "url": "./static/js/2.1362cca8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.1362cca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0d075b79ee7f1148f1",
+    "url": "./static/js/main.f609d197.chunk.js"
+  },
+  {
+    "revision": "1de030c2de9ea21fb0a6",
+    "url": "./static/js/runtime-main.7451e7c7.js"
+  }
+]);
